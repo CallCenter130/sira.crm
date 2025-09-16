09fe9453e6799b421a01ef025c99427ec8e263f7
CallCenter130 <callcenter@mtps.gob.sv>
2025-09-15 21:40:47 -0600

    fix: Resolve remaining merge conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
-/* <source */
+// src/main.tsx
 
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import ReactDOM from 'react-dom/client';
+import App from './pages/App.tsx'; // Asegúrate de que apunte a App.tsx
+
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import appTheme from '../theme';
-import '../index.css';
- 
-const container = document.getElementById('root');
-const root = createRoot(container!);
- 
-root.render(
+import appTheme from '../theme.ts'; 
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ThemeProvider theme={appTheme}>
