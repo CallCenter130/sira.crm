239d23f6dd32f3e1effe14321b62eb44f3466cc0
CallCenter130 <callcenter@mtps.gob.sv>
2025-09-15 20:00:29 -0600

    fix

---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,9 @@
+// src/pages/DashboardPage.jsx
+
+import { Typography } from '@mui/material';
+
+const DashboardPage = () => {
+  return <Typography variant="h4">Dashboard</Typography>;
+};
+
+export default DashboardPage;
