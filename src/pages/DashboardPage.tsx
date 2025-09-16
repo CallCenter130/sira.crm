4b39c80bf05cc8e725261a1072b96fa4687bb479
CallCenter130 <callcenter@mtps.gob.sv>
2025-09-15 18:08:19 -0600

    Fix: Corregido error de sintaxis en NewCasePage

--- src/pages/DashboardPage.tsx
+++
@@ -1,9 +0,0 @@
-// src/pages/DashboardPage.jsx
-
-import { Typography } from '@mui/material';
-
-const DashboardPage = () => {
-  return <Typography variant="h4">Dashboard</Typography>;
-};
-
-export default DashboardPage;
